1edbf1fc5ed16838b607c39512e9826e1785a213
JacopoDaeli <jacopo.daeli@gmail.com>
2016-02-24 11:19:02 +0000

    Update example.js

--- example.js
+++ example.js
@@ -1,9 +1,11 @@
 'use strict'
 
 const fs = require('fs')
-const utp = require('utp')
+const utp = require('utp-native')
 const lps = require('length-prefixed-stream')
 
+let stats = {}
+stats.nDecoderOnData = 0
 
 // RECEIVER
 const writablePath = '/path/to/file/to/write'
@@ -13,15 +15,27 @@ const receiver = utp.createServer(connection => {
   const decoder = lps.decode()
 
   decoder.on('data', chunk => {
-    if (chunk !=== 'END_OF_STREAM') {
+    console.log(`${stats.nDecoderOnData++}: Receiving data from Decoder`)
+    if (chunk.toString() === 'END_OF_STREAM') {
+      // console.log('CLOSING')
       writableStream.end()
-      connection.end()
+      decoder.end()
+      connection.destroy()
     } else {
       writableStream.write(chunk)
     }
   })
 
-  connection.pipe(decoder)
+  connection.on('data', chunk => {
+    // console.log('Receiving data from Socket')
+    decoder.write(chunk)
+  })
+
+  connection.on('close', () => {
+    console.log('Transfer complete')
+  })
+
+  // connection.pipe(decoder)
 })
 
 receiver.listen(listenPort, () => console.log(`Server able to receive files on port ${listenPort}`))
@@ -38,6 +52,7 @@ readableStream.on('data', chunk => {
 })
 
 readableStream.on('end', () => {
+  console.log('SENDING END_OF_STREAM')
   encoder.write('END_OF_STREAM')
 })
 
